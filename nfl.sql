263e8715bfb6df3b3e393f63bf0ed1cc57be803b
Arielle Gironza <ariellegironza@Arielles-MacBook-Air.local>
2023-10-23 12:59:15 -0400

    q7, q8

--- nfl.sql
+++ nfl.sql
@@ -18,13 +18,15 @@ SELECT name, head_coach FROM teams
   OR conference = 'AFC' AND division = 'East';
 
 -- 6. The 50 players with the highest salaries
-
+SELECT name, salary FROM players
+  ORDER BY salary DESC LIMIT 50;
 
 -- 7. The average salary of all NFL players
-
+SELECT AVG(salary) FROM players;
 
 -- 8. The names and positions of players with a salary above 10_000_000
-
+SELECT name, position FROM players
+  WHERE salary > 10000000;
 
 -- 9. The player with the highest salary in the NFL
 
